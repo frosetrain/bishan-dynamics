53d62e3895c27644098d064eb9f7cd2d24984356
vs358 <95121785+sriram358@users.noreply.github.com>
2023-07-21 14:46:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,24 +2,24 @@ from pybricks.hubs import PrimeHub
 from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
 from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase
-from pybricks.tools import wait, StopWatch
+from pybricks.tools import wait
 
 hub = PrimeHub()
 
 left_motor = Motor(Port.B, positive_direction=Direction.COUNTERCLOCKWISE)
 right_motor = Motor(Port.F, positive_direction=Direction.CLOCKWISE)
-third_motor = Motor(Port.C, positive_direction=Direction.CLOCKWISE)
+third_motor = Motor(Port.C, positive_direction=Direction.COUNTERCLOCKWISE)
 left_sensor = ColorSensor(Port.A)
 right_sensor = ColorSensor(Port.E)
 side_sensor = ColorSensor(Port.D)
-side_sensor.detectable_colors((Color.WHITE, Color.BLACK, Color.NONE))
+side_sensor.detectable_colors((Color.RED, Color.NONE))
 db = DriveBase(left_motor, right_motor, 56, 160)
 db.settings(straight_acceleration=1500, turn_rate=300, turn_acceleration=1000)
 
 WHITE = 93
 BLACK = 10
 
-SLOT_TIMES = [
+_SLOT_TIMES = [
     [443, 697],
     [784, 1035],
     [1104, 1364],
@@ -28,6 +28,18 @@ SLOT_TIMES = [
     [2615, 2869],
     [2938, 3167],
     [3258, 3500],
+]
+
+# Positions of slots
+SLOT_ANGLES = [
+    [202, 278],
+    [302, 378],
+    [402, 478],
+    [502, 578],
+    [758, 834],
+    [858, 934],
+    [958, 1034],
+    [1058, 1134],
 ]
 
 tallies = {
@@ -43,8 +55,26 @@ tallies = {
 }
 
 
+def attempt():
+    left_motor.reset_angle(0)
+    slot = 0
+    pthing = Color.NONE
+
+    while True:
+        rl = (right_sensor.reflection() - BLACK) / (WHITE - BLACK)
+        thing = side_sensor.color()
+        if thing == Color.RED and pthing == Color.NONE:
+            pthing = thing
+            print("on", left_motor.angle())
+        elif thing == Color.NONE and pthing == Color.RED:
+            pthing = thing
+            print("off", left_motor.angle())
+        db.drive(150, (rl - 0.5) * 100)
+        
+
+
 def detect():
-    stopwatch = StopWatch()
+    left_motor.reset_angle(0)
     slot = 0
 
     while True:
@@ -53,6 +83,7 @@ def detect():
         rl = (right_sensor.reflection() - BLACK) / (WHITE - BLACK)
         # sc = side_sensor.color()
         sr = side_sensor.reflection()
+        left_motor_angle = left_motor.angle()
         if sr <= 3:
             sc = Color.NONE
         elif sr <= 30:
@@ -61,8 +92,8 @@ def detect():
             sc = Color.WHITE
 
         if (
-            stopwatch.time() > SLOT_TIMES[slot][0]
-            and stopwatch.time() < SLOT_TIMES[slot][1]
+            left_motor_angle > SLOT_ANGLES[slot][0]
+            and left_motor_angle < SLOT_ANGLES[slot][1]
         ):
             if sc == Color.NONE:
                 tallies[slot][0] += 1
@@ -70,11 +101,12 @@ def detect():
                 tallies[slot][1] += 1
             if sc == Color.WHITE:
                 tallies[slot][2] += 3
-        if stopwatch.time() > SLOT_TIMES[slot][1]:
+
+        if left_motor_angle > SLOT_ANGLES[slot][1]:
             slot += 1
             if slot == 8:
                 break
-        db.drive(150, (rl - 0.5) * 100)
+        db.drive(200, (rl - 0.5) * 100)
         # db.drive(100, (ll - rl) * 69)
     print(tallies)
     for i in range(8):
